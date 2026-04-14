d9fd5c43e69a5dccbb4c5f9c86b69ebd5fe163bd
carlos <carloslonghi.cl@gmail.com>
2026-04-14 16:41:53 -0300

    initial commit

---
+++ src/main/java/br/com/carloslonghi/api_chat_ai/ApiChatAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.carloslonghi.api_chat_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChatAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiChatAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carloslonghi/api_chat_ai/ApiChatAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carloslonghi.api_chat_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChatAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
